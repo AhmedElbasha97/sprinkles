68ceb1b663f7648c8e88861d3f3fe01f813028a0
Ahmed elbasha <64855329+AhmedElbasha97@users.noreply.github.com>
2023-07-26 17:02:21 +0300

    Update welcome_screen_ui.dart

--- lib/ui/welcome_screen/welcome_screen_ui.dart
+++ lib/ui/welcome_screen/welcome_screen_ui.dart
@@ -37,182 +37,204 @@ class _WelcomeScreenUIState extends State<WelcomeScreenUI> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(
-                height: Get.height*0.3,
-                width: Get.width*0.8,
-                child: Image.asset("assets/images/logo sprinkles.png",fit: BoxFit.fitHeight,),
-              ),
-              const SizedBox(height: 10,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Column(
+
                 children: [
-                  Container(
-                    height: 60,
-                    width:Get.width*0.35,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: Border.all( color:Colors.white.withOpacity(0.2),width: 5),
-                        boxShadow: [
-                          const BoxShadow(
-                            color: Colors.black12,
-                            offset: Offset(
-                              0.0,
-                              0.0,
-                            ),
-                            blurRadius: 13.0,
-                            spreadRadius: 2.0,
-                          ), //BoxShadow
-                          BoxShadow(
-                            color: Colors.white.withOpacity(0.2),
-                            offset: const Offset(0.0, 0.0),
-                            blurRadius: 0.0,
-                            spreadRadius: 0.0,
-                          ), //BoxShadow
-                        ],
-                        color: Colors.white.withOpacity(0.2)
-                    ),
-                    child:  Container(
-                height: 50,
-                width:Get.width*0.34,
-                      padding: const EdgeInsets.all(0),
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            const BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(
-                                0.0,
-                                0.0,
-                              ),
-                              blurRadius: 13.0,
-                              spreadRadius: 2.0,
-                            ), //BoxShadow
-                            BoxShadow(
-                              color: Colors.white.withOpacity(0.2),
-                              offset: const Offset(0.0, 0.0),
-                              blurRadius: 0.0,
-                              spreadRadius: 0.0,
-                            ), //BoxShadow
-                          ],
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.white.withOpacity(0.2)
-                      ),
-                child: Center(
-                  child: Stack(
+                  SizedBox(
+                    height: Get.height*0.3,
+                    width: Get.width*0.8,
+                    child: Image.asset("assets/images/logo sprinkles.png",fit: BoxFit.fitHeight,),
+                  ),
+                  const SizedBox(height: 10,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // Implement the stroke
-                      CustomText(
-                        'عربي',
-                        style: TextStyle(
-                          fontSize: 25,
-                          letterSpacing: 0,
-                          fontFamily: fontFamilyArabicName,
-                          foreground: Paint()
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth =3
-                            ..color = kBackGroundColor,
+                      Container(
+                        height: 60,
+                        width:Get.width*0.35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all( color:Colors.white.withOpacity(0.2),width: 4),
+                            boxShadow: [
+                              const BoxShadow(
+                                color: kLightPinkColor,
+                                offset: Offset(
+                                  0.0,
+                                  0.0,
+                                ),
+                                blurRadius: 13.0,
+                                spreadRadius: 2.0,
+                              ), //BoxShadow
+                              BoxShadow(
+                                color: Colors.white.withOpacity(0.2),
+                                offset: const Offset(0.0, 0.0),
+                                blurRadius: 0.0,
+                                spreadRadius: 0.0,
+                              ), //BoxShadow
+                            ],
+                            color: Colors.white.withOpacity(0.2)
+                        ),
+                        child:  Container(
+                          height: 50,
+                          width:Get.width*0.34,
+                          padding: const EdgeInsets.all(0),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                const BoxShadow(
+                                  color: Colors.black12,
+                                  offset: Offset(
+                                    0.0,
+                                    0.0,
+                                  ),
+                                  blurRadius: 13.0,
+                                  spreadRadius: 2.0,
+                                ), //BoxShadow
+                                BoxShadow(
+                                  color: Colors.white.withOpacity(0.2),
+                                  offset: const Offset(0.0, 0.0),
+                                  blurRadius: 0.0,
+                                  spreadRadius: 0.0,
+                                ), //BoxShadow
+                              ],
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white.withOpacity(0.2)
+                          ),
+                          child: Center(
+                            child: Stack(
+                              children: [
+                                // Implement the stroke
+                                CustomText(
+                                  'عربي',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    letterSpacing: 0,
+                                    fontFamily: fontFamilyArabicName,
+                                    foreground: Paint()
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeWidth =3
+                                      ..color = kBackGroundColor,
+                                  ),
+                                ),
+                                // The text inside
+                                const CustomText(
+                                  'عربي',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    letterSpacing: 0,
+                                    fontFamily: fontFamilyArabicName,
+                                    color: kDarkPinkColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                      // The text inside
-                      const CustomText(
-                        'عربي',
-                        style: TextStyle(
-                          fontSize: 25,
-                          letterSpacing: 0,
-                          fontFamily: fontFamilyArabicName,
-                          color: kDarkPinkColor,
+                      const SizedBox(width: 20,),
+                      Container(
+                        height: 60,
+                        width:Get.width*0.35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all( color:Colors.white.withOpacity(0.2),width: 4),
+                            boxShadow: [
+                              const BoxShadow(
+                                color: kLightPinkColor,
+                                offset: Offset(
+                                  0.0,
+                                  0.0,
+                                ),
+                                blurRadius: 13.0,
+                                spreadRadius: 2.0,
+                              ), //BoxShadow
+                              BoxShadow(
+                                color: Colors.white.withOpacity(0.2),
+                                offset: const Offset(0.0, 0.0),
+                                blurRadius: 0.0,
+                                spreadRadius: 0.0,
+                              ), //BoxShadow
+                            ],
+                            color: Colors.white.withOpacity(0.2)
+                        ),
+                        child:  Container(
+                          height: 50,
+                          width:Get.width*0.34,
+                          padding: const EdgeInsets.all(0),
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                const BoxShadow(
+                                  color: Colors.black12,
+                                  offset: Offset(
+                                    0.0,
+                                    0.0,
+                                  ),
+                                  blurRadius: 13.0,
+                                  spreadRadius: 2.0,
+                                ), //BoxShadow
+                                BoxShadow(
+                                  color: Colors.white.withOpacity(0.2),
+                                  offset: const Offset(0.0, 0.0),
+                                  blurRadius: 0.0,
+                                  spreadRadius: 0.0,
+                                ), //BoxShadow
+                              ],
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white.withOpacity(0.2)
+                          ),
+                          child: Center(
+                            child: Stack(
+                              children: [
+                                // Implement the stroke
+                                CustomText(
+                                  'English',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    letterSpacing: 0,
+                                    fontFamily: fontFamilyEnglishName,
+                                    fontWeight: FontWeight.w900,
+                                    foreground: Paint()
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeWidth =3
+                                      ..color = kBackGroundColor,
+                                  ),
+                                ),
+                                // The text inside
+                                const CustomText(
+                                  'English',
+                                  style: TextStyle(
+                                    fontSize: 25,
+                                    letterSpacing: 0,
+                                    fontFamily: fontFamilyEnglishName,
+                                    fontWeight: FontWeight.w900,
+                                    color: kDarkPinkColor,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ),
-                  ),
-                  const SizedBox(width: 20,),
-                  Container(
-                    height: 60,
-                    width:Get.width*0.35,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                      border: Border.all( color:Colors.white.withOpacity(0.2),width: 5),
-                        boxShadow: [
-                          const BoxShadow(
-                            color: Colors.black12,
-                            offset: Offset(
-                              0.0,
-                              0.0,
-                            ),
-                            blurRadius: 13.0,
-                            spreadRadius: 2.0,
-                          ), //BoxShadow
-                          BoxShadow(
-                            color: Colors.white.withOpacity(0.2),
-                            offset: const Offset(0.0, 0.0),
-                            blurRadius: 0.0,
-                            spreadRadius: 0.0,
-                          ), //BoxShadow
-                        ],
-                      color: Colors.white.withOpacity(0.2)
-                    ),
-                    child:  Container(
-                      height: 50,
-                      width:Get.width*0.34,
-                      padding: const EdgeInsets.all(0),
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            const BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(
-                                0.0,
-                                0.0,
-                              ),
-                              blurRadius: 13.0,
-                              spreadRadius: 2.0,
-                            ), //BoxShadow
-                            BoxShadow(
-                              color: Colors.white.withOpacity(0.2),
-                              offset: const Offset(0.0, 0.0),
-                              blurRadius: 0.0,
-                              spreadRadius: 0.0,
-                            ), //BoxShadow
-                          ],
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.white.withOpacity(0.2)
-                      ),
-                      child: Center(
-                        child: Stack(
-                          children: [
-                            // Implement the stroke
-                            CustomText(
-                              'English',
-                              style: TextStyle(
-                                fontSize: 25,
-                                letterSpacing: 0,
-                                fontFamily: fontFamilyEnglishName,
-                                fontWeight: FontWeight.w900,
-                                foreground: Paint()
-                                  ..style = PaintingStyle.stroke
-                                  ..strokeWidth =3
-                                  ..color = kBackGroundColor,
-                              ),
-                            ),
-                            // The text inside
-                             const CustomText(
-                              'English',
-                              style: TextStyle(
-                                fontSize: 25,
-                                letterSpacing: 0,
-                                fontFamily: fontFamilyEnglishName,
-                                fontWeight: FontWeight.w900,
-                                color: kDarkPinkColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               ),
+
+
+              Padding(
+                padding: const EdgeInsets.symitric(8.0),
+                child: Stack(
+                  children:[
+                    Container(
+                      height: Get.height*0.3,
+                      width:Get.width,
+                      color:Colors.white,
+                      child:const Padding(padding: EdgeInsets.all(0),
+
+                      )
+                    )
+                  ]
+                ),
+              )
             ],
           ),
         ),
